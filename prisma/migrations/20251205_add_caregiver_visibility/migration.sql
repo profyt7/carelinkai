78690b90762a361c5de41ee432b82fc0227fa240
Chris tolliver <profyt7@gmail.com>
2025-12-05 01:44:08 -0500

    chore(db): add migration for caregiver marketplace visibility flag [Droid-assisted]

---
+++ prisma/migrations/20251205_add_caregiver_visibility/migration.sql
@@ -0,0 +1,6 @@
+﻿-- AlterTable
+ALTER TABLE "Caregiver" ADD COLUMN     "isVisibleInMarketplace" BOOLEAN NOT NULL DEFAULT true;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "FamilyMember_familyId_userId_key" ON "FamilyMember"("familyId", "userId");
+
